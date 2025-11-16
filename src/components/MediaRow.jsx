f1203b150a1ab3769116203bbd965d71aac88d0a
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-16 14:27:43 +0200

    remove unused button element from MediaRow

--- src/components/MediaRow.jsx
+++ src/components/MediaRow.jsx
@@ -1,9 +1,6 @@
 import {Link} from 'react-router';
 
 const MediaRow = ({item}) => {
-  //console.log(props);
-  //const {item, updateSelectedItem} = props;
-
   return (
     <tr key={item.media_id}>
       <td>
@@ -11,13 +8,11 @@ const MediaRow = ({item}) => {
       </td>
       <td>{item.title}</td>
       <td>{item.description}</td>
+      <td>{item.username}</td>
       <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
       <td>{item.filesize}</td>
       <td>{item.media_type}</td>
       <td>
-        {/* <button onClick={() => updateSelectedItem(item)}>
-          Display Content
-        </button> */}
         {
           <Link to="/single" state={item}>
             Show
