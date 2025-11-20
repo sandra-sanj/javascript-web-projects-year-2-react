da2a378a1024eb59f462c981ab5e8bc145926737
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-20 14:52:50 +0200

    add login and logout paths to Layout.jsx

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -15,6 +15,15 @@ const Layout = () => {
           <li>
             <Link to="/upload">Upload</Link>
           </li>
+          <li>
+            <Link to="/pizza">Pizza</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
         </ul>
       </nav>
       <main>
