392664fa76740d95109a761821e2b5dd1664839d
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-20 12:46:12 +0200

    add header to Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -9,10 +9,10 @@ const Home = () => {
   const [selectedItem, setSelectedItem] = useState(null);
 
   const {mediaArray} = useMedia();
-
   return (
     <>
       <SingleView item={selectedItem} setSelectedItem={setSelectedItem} />
+      <h2>My Media</h2>
       <table>
         <thead>
           <tr>
