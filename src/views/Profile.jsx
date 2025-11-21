7e2aa1ed7f49204a09ad56197deabb15b1239cfe
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-21 10:12:19 +0200

    add user info to Profile.jsx

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,16 +1,45 @@
-import PropTypes from 'prop-types';
-import HookDemo from '../components/HookDemo';
+import {useEffect, useState} from 'react';
+import {useUser} from '../hooks/apiHooks';
 
 const Profile = () => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState('');
+  const {getUserByToken} = useUser();
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const userResponse = await getUserByToken(
+          localStorage.getItem('token'),
+        );
+        setUser(userResponse.user);
+      } catch (error) {
+        setError(error.message);
+        console.log('getUserData error', error);
+      }
+    };
+    getUserData();
+  }, []);
+  console.log('user', user);
+
   return (
     <>
       <h2>Profile</h2>
-      <div>Hook Demo</div>
-      <HookDemo />
+      <hr />
+      {user && (
+        <>
+          <p>Username: {user.username}</p>
+          <p>Email: {user.email}</p>
+          <p>Registered at: {user.created_at}</p>
+        </>
+      )}
+      {error && (
+        <>
+          <p>Profile information loading failed. {error}</p>
+        </>
+      )}
     </>
   );
 };
-
-Profile.propTypes = {};
 
 export default Profile;
