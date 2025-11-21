cb2bd04929f0755e4e2bf9be3d0a0daa9bde3d87
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-21 10:57:48 +0200

    add conditional rendering to Profile in Profile.jsx

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -26,16 +26,18 @@ const Profile = () => {
     <>
       <h2>Profile</h2>
       <hr />
-      {user && (
+      {user ? (
         <>
           <p>Username: {user.username}</p>
           <p>Email: {user.email}</p>
           <p>Registered at: {user.created_at}</p>
         </>
+      ) : (
+        <p>Loading profile information...</p>
       )}
       {error && (
         <>
-          <p>Profile information loading failed. {error}</p>
+          <p>Loading profile information failed. {error}</p>
         </>
       )}
     </>
