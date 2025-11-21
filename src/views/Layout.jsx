484b8cfe9c23717b1808ca3bccabe65b1d3c6f8a
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-21 13:19:38 +0200

    add conditional rendering to links in Layout.jsx

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,7 +1,15 @@
 import PropTypes from 'prop-types';
 import {Link, Outlet} from 'react-router';
+import {useUserContext} from '../hooks/contextHooks';
+import {useEffect} from 'react';
 
 const Layout = () => {
+  const {handleAutoLogin, user} = useUserContext();
+
+  useEffect(() => {
+    handleAutoLogin();
+  }, []);
+
   return (
     <div>
       <nav>
@@ -10,20 +18,27 @@ const Layout = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/upload">Upload</Link>
-          </li>
-          <li>
             <Link to="/pizza">Pizza</Link>
           </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/logout">Logout</Link>
-          </li>
+
+          {user && (
+            <>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/upload">Upload</Link>
+              </li>
+              <li>
+                <Link to="/logout">Logout</Link>
+              </li>
+            </>
+          )}
+          {!user && (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
         </ul>
       </nav>
       <main>
