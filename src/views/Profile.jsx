58db22615a19ab444301452317ddf38d8cf6bd96
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-28 18:12:26 +0200

    add tailwind classes to Profile.jsx

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -5,17 +5,26 @@ const Profile = () => {
   const [error, setError] = useState('');
   const {user} = useUserContext();
 
-  console.log('user', user);
+  //console.log('user', user);
 
   return (
     <>
-      <h2>Profile</h2>
+      <h2 className="bg-green-700 text-cyan-200 w-auto hover:bg-amber-200 hover:text-amber-900 text-3xl">
+        Profile
+      </h2>
+
       <hr />
       {user ? (
         <>
-          <p>Username: {user.username}</p>
-          <p>Email: {user.email}</p>
-          <p>Registered at: {user.created_at}</p>
+          <div className="p-7">
+            <div className="card justify-self-center border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
+              <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">
+                Username: {user.username}
+              </h5>
+              <p className="text-body">Email: {user.email}</p>
+              <p className="text-body">Registered at: {user.created_at}</p>
+            </div>
+          </div>
         </>
       ) : (
         <p>Loading profile information...</p>
